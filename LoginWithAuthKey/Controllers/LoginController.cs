ef5bcacc0790992af8e2316c64c172d7cb8830c3
tavisca-rtiwari <51901929+tavisca-rtiwari@users.noreply.github.com>
2019-10-04 15:27:44 +0530

    token based Authentication Api Completed

---
+++ LoginWithAuthKey/Controllers/LoginController.cs
@@ -0,0 +1,103 @@
+﻿using LoginWithAuthKey.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private IConfiguration _config;
+        private UserManager<UserModel> _userManager;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpGet]
+        //public ActionResult<IEnumerable<string>> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody]LoginModel login)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+
+        }
+        private string GenerateJSONWebToken(UserModel userInfo)
+        {
+            var role = userInfo.Roll;
+            IdentityOptions _options = new IdentityOptions();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                   {
+                        new Claim("UserID",userInfo.Id.ToString()),
+                        new Claim(_options.ClaimsIdentity.RoleClaimType,role.ToString())
+                   }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"])), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.WriteToken(securityToken);
+            return token;
+
+            /*  var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Issuer"],
+                null,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+              return new JwtSecurityTokenHandler().WriteToken(token);*/
+        }
+
+        private UserModel AuthenticateUser(LoginModel login)
+        {
+            UserModel user = null;
+
+            //Validate the User Credentials  
+            //Demo Purpose, I have Passed HardCoded User Information  
+            if (login.UserName == "Rahul" && login.Password == "12345678")
+            {
+                user = new UserModel()
+                {
+                    UserName = "Rahul",
+                    Email = "r@gmail.com",
+                    FullName = "Rahul Tiwari",
+                    Roll = "Admin"
+                };
+            }
+            return user;
+        }
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        [Route("Value")]
+        public ActionResult<IEnumerable<string>> GetValue()
+        {
+            return new string[] { "value1", "value2", "value3", "value4", "value5" };
+        }
+
+    }
+}
---
+++ LoginWithAuthKey/Models/AuthenticateUser.cs
@@ -0,0 +1,32 @@
+﻿using JWTAuthentication.Controllers;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginWithAuthKey.Models
+{
+    
+        public class AuthenticateUser : IdentityDbContext
+    {
+        public AuthenticateUser(DbContextOptions options) : base(options)
+        {
+            ApplicationUsers.Add(new UserModel(){UserName = "Rahul",
+                Email = "r@gmail.com",
+                FullName = "Rahul Tiwari",
+                Roll = "Admin"});
+            ApplicationUsers.Add(new UserModel()
+            {
+                UserName = "Prakhar",
+                Email = "p@gmail.com",
+                FullName = "Prakhar Gupta",
+                Roll = "Trainee"
+            });
+        }
+
+        public DbSet<UserModel> ApplicationUsers { get; set; }
+        
+    }
+}
